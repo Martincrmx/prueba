5df6babdd691056f349a5b4f6dbc7ad1962047e3
Martincrmx <martin_crxd@live.com.mx>
2022-09-04 15:00:28 -0500

    Obtener usuarios
    Obtenemos los usuarios de la función consultar usuarios

--- includes/consultas.php
+++ includes/consultas.php
@@ -1,14 +1,21 @@
 <?php 
 
+// VARIABLES INICIALES
+$consultar_url = "https://reqres.in/api/users/"; // ALMACENAMOS LA VARIABLE A CONSULTAR
+
 // INICIAMOS LA LLAMADA A LA CLASE CONSULTA
 $consulta = new Consulta(); 
 // LLAMAMOS A LA FUNCIÓN CONSULTAR USUARIOS
-$consulta->ConsultarUsuarios();
+$usuarios = $consulta->ConsultarUsuarios($consultar_url);
 
 	Class Consulta{
 
-		public function ConsultarUsuarios(){
+		public function ConsultarUsuarios($url){
 			
+			$resultado = file_get_contents($url); // UTILIZAMOS LA FUNCION FILES GETS CONTENT PARA CONSULTAR LA URL
+			$resultadoDecodificado = json_decode($resultado , true); // EL RESULTADO NOS DEVUELVE UN JSON EL CUAL DECODIFICAMOS Y ALMACENAMOS EL RESULTADO
+			$usuarios = $resultadoDecodificado['data']; // ALMACENAMOS LA DATA EN UNA VARIABLE PARA RETORNAR LA FUNCION Y MOSTRAR LOS VALORES
+			return $usuarios;
 		}
 	}
 ?>
