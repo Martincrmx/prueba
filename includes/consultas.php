8ffb810eb254d2de29ecdf65c5ecdddff1ad5166
Martincrmx <martin_crxd@live.com.mx>
2022-09-04 17:04:59 -0500

    Armando estructura html parte1
    Comienzo del armado de la estructura html

--- includes/consultas.php
+++ includes/consultas.php
@@ -1,12 +1,17 @@
 <?php 
 
-// VARIABLES INICIALES
-$consultar_url = "https://reqres.in/api/users/"; // ALMACENAMOS LA VARIABLE A CONSULTAR
 
-// INICIAMOS LA LLAMADA A LA CLASE CONSULTA
-$consulta = new Consulta(); 
-// LLAMAMOS A LA FUNCIÓN CONSULTAR USUARIOS
-$usuarios = $consulta->ConsultarUsuarios($consultar_url);
+if(!$_POST){
+	// VARIABLES INICIALES
+	$consultar_url = "https://reqres.in/api/users/"; // ALMACENAMOS LA VARIABLE A CONSULTAR
+
+	// INICIAMOS LA LLAMADA A LA CLASE CONSULTA
+	$consulta = new Consulta(); 
+	// LLAMAMOS A LA FUNCIÓN CONSULTAR USUARIOS
+	$usuarios = $consulta->ConsultarUsuarios($consultar_url);
+}
+
+
 
 	Class Consulta{
 
--- index.php
+++ index.php
@@ -1,20 +1,40 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	<title>Ejercicio</title>
 	<!-- LLAMADA AL ARCHIVO CONSULTA -->
 	<?php require_once('includes/consultas.php'); ?>
 	<!-- HOJAS DE ESTILO -->
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="css/style_gral.css">
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.12.1/datatables.min.css"/>
 </head>
 <body>
+	<header>
+		<div id="header">
+			<div class="container">
+				<div class="row">
+					<div id="logo" class="col-md-3 col-xs-12">
+						<img src="img/logo.png">
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
 	<div class="container">
+		
 		<div class="row">
 			<div class="col-md-12">
-				<h1>Ejercicio</h1>
+				<div class="usuarios-box">
+				</div>
 			</div>
 		</div>
 	</div>
+	<!-- INCLUIR SCRIPTS -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.12.1/datatables.min.js"></script>
+
 </body>
 </html>
