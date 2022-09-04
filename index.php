fc0c30980ccf7d3d66cb5b3f4c881892db24211d
Martincrmx <martin_crxd@live.com.mx>
2022-09-04 13:29:59 -0500

    Agregando hojas de estilo
    Se agregaron las principales hojas de estilo

--- index.php
+++ index.php
@@ -2,8 +2,18 @@
 <html>
 <head>
 	<title>Ejercicio</title>
+
+	<!-- HOJAS DE ESTILO -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style_gral.css">
 </head>
 <body>
-
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Ejercicio</h1>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
